44ebd1e8531b62208b1d76f8e8cc6fa43a45c799
Dayat222 <dayat.cyberzeroone@gmail.com>
2026-09-21 21:10:47 +0800

    build: increase chunk size warning limit and add manual chunks for optimization

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,18 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1500, // Matikan peringatan batas ukuran file
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Pisahkan library besar agar loading web lebih cepat
+          vendor: ['react', 'react-dom'],
+          icons: ['lucide-react'],
+          media: ['hls.js'],
+          network: ['mqtt']
+        }
+      }
+    }
+  }
 })
